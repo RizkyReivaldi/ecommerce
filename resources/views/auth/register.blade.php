9b1b1e56d6f6b039ff545b527976b4e1b3a48199
RizkyReivaldi <funnehc90@gmail.com>
2025-12-17 15:37:33 +0700

    Authentication dengan Laravel UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,6 +68,42 @@
                                 </button>
                             </div>
                         </div>
+                        <div class="mt-3 text-center">
+                            @if (Route::has('password.request'))
+                            <a
+                                class="text-decoration-none"
+                                href="{{ route('password.request') }}"
+                            >
+                                Lupa Password?
+                            </a>
+                            @endif
+                        </div>
+
+                        <hr />
+                        {{-- ↑ Garis pemisah --}} {{-- ================== SOCIAL LOGIN
+                        ================== --}} {{-- Tombol ini akan diaktifkan di Hari 4
+                        --}}
+                        <div class="d-grid gap-2">
+                            <a href="#" class="btn btn-outline-danger">
+                                <img
+                                src="https://www.svgrepo.com/show/475656/google-color.svg"
+                                width="20"
+                                class="me-2"
+                                />
+                                Login dengan Google
+                            </a>
+                        </div>
+
+                        {{-- ================== LINK REGISTER ================== --}}
+                        <p class="mt-4 text-center mb-0">
+                            Belum punya akun?
+                            <a
+                                href="{{ route('register') }}"
+                                class="text-decoration-none fw-bold"
+                            >
+                                Daftar Sekarang
+                            </a>
+                        </p>
                     </form>
                 </div>
             </div>
